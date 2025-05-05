f79e652d9d8c5dbcee1c28c10a8e64486f96024c
lennardevertz <levertz@idriss.xyz>
2025-05-05 14:12:24 +0200

    simplify voting token

--- contracts/UMA/data-verification-mechanism/implementation/VotingToken.sol
+++ contracts/UMA/data-verification-mechanism/implementation/VotingToken.sol
@@ -2,28 +2,25 @@
 pragma solidity ^0.8.0;
 
 import "../../common/implementation/ExpandedERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
 
 /**
  * @title Ownership of this token allows a voter to respond to price requests.
  * @dev Supports snapshotting and allows the Oracle to mint new tokens as rewards.
  */
-contract VotingToken is ExpandedERC20, ERC20Snapshot {
+contract VotingToken is ExpandedERC20 {
     /**
      * @notice Constructs the VotingToken.
      */
-    constructor() ExpandedERC20("UMA Voting Token v1", "UMA", 18) ERC20Snapshot() {}
+    constructor() ExpandedERC20("UMA Voting Token v1", "UMA", 18) {}
 
-    function decimals() public view virtual override(ERC20, ExpandedERC20) returns (uint8) {
+    function decimals()
+        public
+        view
+        virtual
+        override(ExpandedERC20)
+        returns (uint8)
+    {
         return super.decimals();
-    }
-
-    /**
-     * @notice Creates a new snapshot ID.
-     * @return uint256 Thew new snapshot ID.
-     */
-    function snapshot() external returns (uint256) {
-        return _snapshot();
     }
 
     // _transfer, _mint and _burn are ERC20 internal methods that are overridden by ERC20Snapshot,
@@ -38,19 +35,17 @@ contract VotingToken is ExpandedERC20, ERC20Snapshot {
         super._transfer(from, to, value);
     }
 
-    function _mint(address account, uint256 value) internal virtual override(ERC20) {
+    function _mint(
+        address account,
+        uint256 value
+    ) internal virtual override(ERC20) {
         super._mint(account, value);
     }
 
-    function _burn(address account, uint256 value) internal virtual override(ERC20) {
+    function _burn(
+        address account,
+        uint256 value
+    ) internal virtual override(ERC20) {
         super._burn(account, value);
-    }
-
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual override(ERC20, ERC20Snapshot) {
-        super._beforeTokenTransfer(from, to, amount);
     }
 }
