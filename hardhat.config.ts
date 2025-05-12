d1b25c55677f262c4b7f942655055e26869e927b
lennardevertz <levertz@idriss.xyz>
2025-05-12 11:32:44 +0200

    rm unused libs and interfaces

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,14 @@
-import type { HardhatUserConfig } from "hardhat/config";
+import type {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+    solidity: {
+        compilers: [
+            {version: "0.8.16"},
+            {version: "0.8.20"},
+            {version: "0.8.28"},
+        ],
+    },
 };
 
 export default config;
